c2e9d1e81107122598bee2ccb374e70ce38b9f33
Sean Nair <s.j.nair2@gmail.com>
2025-09-06 15:11:02 -0500

    wrote insertion sort

---
+++ java2/module6/MyInsertionSort.java
@@ -0,0 +1,30 @@
+package java2.module6;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class MyInsertionSort {
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[10];
+        Random rand=new Random();
+        for (int i=0; i<arr.length; i++){
+            arr[i]=rand.nextInt(100);
+        }
+        System.out.println(Arrays.toString(arr));
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sort(Comparable[] arr){
+        Comparable temp;
+        for (int i=0; i<arr.length-1; i++){
+            for (int j=i; j>=0; j--){
+                if (arr[j].compareTo(arr[j+1])<0)
+                    break;
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
